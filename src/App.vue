97d8c8f9fdc971107e06971c64bd7a684ec008c0
shshhan <seunghyeonhan95@gmail.com>
2022-02-17 00:31:39 +0900

    v-for

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="menu">
+		<a v-for="(name, i) in menuNames" :key="i">{{name}}</a>
+  </div>
+
+	<div v-for="(name, i) in products" :key="i">
+		<h4>{{name}}</h4>
+		<p>{{price[i]}}</p>
+		<button v-on:click="increaseReportCnt(i)">허위매물신고</button>
+		<span>신고수 : {{reportCnt[i]}} </span>
+	</div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){
+    return {  //데이터 보관함 
+			menuNames : ['Home', 'Shop', 'About'],
+      products : ['논현동원룸', '잠실동원룸', '가락동원룸'],
+      price : [100, 90, 60],
+      h4Color : 'color : blue',
+			reportCnt : [0, 0, 0],
+		}
+  },
+	methods :{
+		increaseReportCnt(i){
+			console.log(i + '번째 항목의 신고수가 1증가됩니다.');
+			this.reportCnt[i]++;
+		},
+	},
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -21,6 +43,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10px;
+}
+
+.menu{
+  background: darkslateblue;
+  padding : 15px;
+  border-radius: 5px;
+}
+
+.menu a{
+  color : white;
+  padding : 10px;
 }
 </style>
